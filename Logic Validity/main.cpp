9f269d7de4ba02b1b0f8117da34d775e66c31c58
Oliver Robson <oliver.t.robson@gmail.com>
2021-04-29 15:14:34 +1200

    Initial Commit

---
+++ Logic Validity/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Logic Validity
+//
+//  Created by Oliver Robson on 29/04/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
