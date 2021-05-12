2dd31e4ed980c46a6602c54cafe74448e1758015
Oliver Robson <oliver.t.robson@gmail.com>
2021-05-12 21:18:44 +1200

    Premises are split down into atoms recursively. Using pointers, and not handling them yet.

--- premise.cpp
+++ premise.cpp
@@ -10,57 +10,75 @@
 #include <iostream>
 #include <regex>
 
-enum connective {Conjunct, Disjunct, Conditional, Biconditional};
+enum connective {Conjunct, Disjunct, Conditional, Biconditional, None};
 
 class Premise {
 private:
     std::string premise;
     connective mainConnective;
+    Premise* left;
+    Premise* right;
 public:
     Premise (std::string p) {
         premise = p;
-        
-        //look for a connective symbol not contained in brackets
-        int c = 0; //keep count of position in string
-        bool connectiveFound = false; //Have we found the main connective?
-        while (c < p.length() && connectiveFound == false) {
-            int b = 0; //keep count of bracket depth
-            if (p[c] == '(') {
-                b++;
-                c++;
-                while (c < p.length() && b > 0) { //progress through brackets
-                    if (p[c] == '(') {
-                        b++;
-                    } else if (p[c] == ')') {
-                        b--;
-                    }
+        if (p.length() == 1) { //The premise is an atom! We're done here.
+            mainConnective = None;
+            delete left;
+            delete right;
+        } else {
+            //look for a connective symbol not contained in brackets
+            int c = 0; //keep count of position in string
+            bool connectiveFound = false; //Have we found the main connective?
+            while (c < p.length() && connectiveFound == false) {
+                int b = 0; //keep count of bracket depth
+                if (p[c] == '(') {
+                    b++;
                     c++;
+                    while (c < p.length() && b > 0) { //progress through brackets
+                        if (p[c] == '(') {
+                            b++;
+                        } else if (p[c] == ')') {
+                            b--;
+                        }
+                        c++;
+                    }
                 }
+                
+                switch (p[c]) { //check for a connective
+                    case '^':
+                        mainConnective = Conjunct;
+                        connectiveFound = true;
+                        break;
+                    case 'v':
+                        mainConnective = Disjunct;
+                        connectiveFound = true;
+                        break;
+                    case '-':
+                        mainConnective = Conditional;
+                        connectiveFound = true;
+                        break;
+                    case '<':
+                        mainConnective = Biconditional;
+                        connectiveFound = true;
+                        break;
+                        
+                }
+                
+                c++;
             }
-            
-            switch (p[c]) { //check for a connective
-                case '^':
-                    mainConnective = Conjunct;
-                    connectiveFound = true;
-                    break;
-                case 'v':
-                    mainConnective = Disjunct;
-                    connectiveFound = true;
-                    break;
-                case '-':
-                    mainConnective = Conditional;
-                    connectiveFound = true;
-                    break;
-                case '<':
-                    mainConnective = Biconditional;
-                    connectiveFound = true;
-                    break;
+            if (p[0] == '(') { //strip brackets
+                left = new Premise(p.substr(1, c-2));
+            } else {
+                left = new Premise(p.substr(0, c-1));
             }
-            
-            c++;
+            if (p[c+1] == '(') { //strip brackets
+                right = new Premise(p.substr(c+2, p.length()-1));
+            } else {
+                right = new Premise(p.substr(c+1, p.length()));
+            }
         }
         
         std::cout << mainConnective << "\n";
-        
+
     }
 };
