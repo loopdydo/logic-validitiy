e4aaaa16a278cfd611320c217c12f43bf556ae64
Oliver Robson <oliver.t.robson@gmail.com>
2021-05-18 19:43:11 +1200

    Premises stored in a vector.

--- main.cpp
+++ main.cpp
@@ -2,17 +2,20 @@
 //  main.cpp
 //  Logic Validity
 //
-//  Created by Oliver Robson on 29/04/21.
+//  Created by Oliver Robson.
 //
 
 #include <iostream>
 #include "premise.cpp"
 #include <string>
+#include <vector>
 
 int main(int argc, const char * argv[]) {
     std::string premise;
+    std::vector<Premise> premises;
     while (getline(std::cin, premise)) {
         Premise p(premise);
+        premises.push_back(p);
     }
     return 0;
 }
